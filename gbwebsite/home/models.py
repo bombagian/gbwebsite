6e6fec924b0c6051740fb8d7361d3b5984792321
Gianfranco Bombardieri <gianfranco@Gianfrancos-MacBook-Pro.local>
2019-08-09 22:52:23 +0100

    first migration

--- gbwebsite/home/models.py
+++ gbwebsite/home/models.py
@@ -3,17 +3,17 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 
+
 class HomePage(Page):
-    
+
     templates = 'home/home_page.html'
     max_count = 1
 
-    banner_title = models.CharField(max_lenght=100, blank=False, null=True)
-    
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
     content_panels = Page.content_panels + [
         FieldPanel('banner_title')
-    ] 
+    ]
 
     class Meta:
-        verbose_name : "Home Page"
-        verbose_name_plural: "Home Pages"
+        verbose_name: 'Home Page'
+        verbose_name_plural: 'Home Pages'
