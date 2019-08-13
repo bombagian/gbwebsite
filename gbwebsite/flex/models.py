acb0595e4a2d8167d6ce8c42fb4b5f4a7c00715d
Gianfranco Bombardieri <gianfranco@Gianfrancos-MacBook-Pro.local>
2019-08-13 20:35:27 +0100

    simple streamField added

--- gbwebsite/flex/models.py
+++ gbwebsite/flex/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+
+from streams import blocks
+
 # Create your models here.
 
 
@@ -9,15 +13,21 @@ class FlexPage(Page):
 
     template = "flex/flex_page.html"
 
-
-    #content = StramField()
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock())
+        ],
+        null=True,
+        blank=True,
+    )
 
     subtitle = models.CharField(max_length=100, null=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
     ]
 
-    class Meta: #noqa
+    class Meta: # noqa
         verbose_name = "Flex Page"
         verbose_name_plural = "Flex Pages"
---
+++ gbwebsite/streams/blocks.py
@@ -0,0 +1,14 @@
+from wagtail.core import blocks
+
+
+class TitleAndTextBlock(blocks.StructBlock):
+    "title and nothing else"
+
+    title = blocks.CharBlock(required=True, help_text='Add your title')
+    text = blocks.TextBlock(required=True, help_text='Add additional text')
+
+
+    class Meta: # noqa
+        template = "streams/title_and_text_block.html"
+        icon = "edit"
+        label = "Title and Text"
