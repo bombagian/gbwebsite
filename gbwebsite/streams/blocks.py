5d72d63136b66875dc1f0611c69b6720f76c9855
Gianfranco Bombardieri <gianfranco@Gianfrancos-MacBook-Pro.local>
2019-08-14 21:34:16 +0100

    richtexblock added

--- gbwebsite/streams/blocks.py
+++ gbwebsite/streams/blocks.py
@@ -12,3 +12,26 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title and Text"
+
+
+class RichtextBlock (blocks.RichTextBlock):
+
+    class Meta:
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full RichText"
+
+
+class SimpleRichtextBlock (blocks.RichTextBlock):
+    def __init__(self, required=True, help_text=None, editor='default', features=None, validators=(), **kwargs):
+        super().__init__(**kwargs)
+        self.features = [
+            "bold",
+            "italic",
+            "link",
+        ]
+
+    class Meta:
+        template = "streams/richtext_block.html"
+        icon = "edit"
+        label = "Simple RichText"
