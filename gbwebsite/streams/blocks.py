2eacb9a4ff8696ad4173197059cf9bfd47e87677
Gianfranco Bombardieri <gianfranco@Gianfrancos-MacBook-Pro.local>
2019-08-14 22:34:28 +0100

    list block added

--- gbwebsite/streams/blocks.py
+++ gbwebsite/streams/blocks.py
@@ -1,4 +1,5 @@
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -12,6 +13,26 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title and Text"
+
+
+class CardBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True, help_text='Add your title')
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(required=True, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False, help_text="if the button page above is selected, that will be used first")),
+            ]
+        )
+    )
+
+    class Meta:
+        template = "streams/card_block.html"
+        icon = "placeholder"
+        label = "Staff Cards"
 
 
 class RichtextBlock (blocks.RichTextBlock):
